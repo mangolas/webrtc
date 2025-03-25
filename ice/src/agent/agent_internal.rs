9e76f31ca59956ce7d1cccde0e9f37dd44b9a445
Lassi Immonen <lassi@anarkylabs.com>
2025-03-25 14:58:01 +0200

    WIP

--- ice/src/agent/agent_internal.rs
+++ ice/src/agent/agent_internal.rs
@@ -556,7 +556,6 @@ impl AgentInternal {
         self: &Arc<Self>,
         c: &Arc<dyn Candidate + Send + Sync>,
     ) -> Result<()> {
-        println!("WTF!!!");
         log::info!("add candidate: {}", c);
         let initialized_ch = {
             let started_ch_tx = self.started_ch_tx.lock().await;
